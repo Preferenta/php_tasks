21eefb04e02a8eb4ad11e83d368499b577fa3aa6
Oksana D <preferenta@gmail.com>
2023-06-09 18:50:11 +0300

    commit

--- Basics/getHiddenCard.php
+++ Basics/getHiddenCard.php
@@ -1,5 +1,5 @@
 <?php
 function getHiddenCard($cardNumber, $starsCount = 4) {
     $visibleNumbers = substr($cardNumber, 12);
-    return str_reapet('*', $starsCount) . $visibleNumbers;
+    return str_repeat('*', $starsCount) . $visibleNumbers;
 }
