f31cc3695625fccbf1edde01039b906d86103611
Oksana D <preferenta@gmail.com>
2023-06-13 18:22:40 +0300

    function addPrefix

--- Array/for.php
+++ Array/for.php
@@ -9,3 +9,12 @@ for($i = 0; $i < count($userNames); $i++) {
     print_r("{$userNames[$i]} \n");
 }
 ?>
+<?php
+
+function addPrefix($names, $prefix) {
+    $result = [];
+    for($i = 0, $length = count($names); $i < $length; $i++) {
+        $result[$i] = "{$prefix} {$names[$i]}";
+    }
+    return $result;
+}
