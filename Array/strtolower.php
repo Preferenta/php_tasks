d0cdc33e04a21d9f76a56372c28ea5b14b08faea
Oksana D <preferenta@gmail.com>
2023-06-13 18:18:21 +0300

    strtolower

--- Array/strtolower.php
+++ Array/strtolower.php
@@ -6,3 +6,16 @@ for ($i = 0; $i < count($emails); $i++) {
     $emails[$i] = strtolower($emails[$i]); 
 }
 print_r($emails);
+
+$colors = ['red', 'blue', 'green', 'yellow'];
+$length = count($colors);
+
+for ($i = 0; $i < $length; $i++) {
+    print_r("{$colors[$i]}");
+}
+
+$colors = ['red', 'blue', 'green', 'yellow'];
+
+for ($i = 0; $length = count($colors); $i < $length; print($i), $i++) {
+    print_r("{$colors[$i]}\n");
+}
