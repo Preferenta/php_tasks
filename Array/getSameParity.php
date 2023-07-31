763d7cc6dd8c2f2e1e48e3000ce44e21c05c15d8
Oksana D <preferenta@gmail.com>
2023-07-31 12:34:42 +0300

    getSameParity

--- Array/getSameParity.php
+++ Array/getSameParity.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
 function getSameParity(array $coll) 
 {
     if (empty($coll)) {
         return [];
     }
     $result = [];
-    $remainder = $coll[0] % 2;
+    $firstElement = $coll[] % 2;
+
     foreach ($coll as $item) {
-        if ($item % 2 === $remainder) {
+        if ($item % 2 === $firstElement) {
             $result[] = $item;
         }
     }
     return $result;
 }
+?>
