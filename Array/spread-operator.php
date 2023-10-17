8a998ff02281af25a7d514e6d6a25256557ab9c8
Oksana D <preferenta@gmail.com>
2023-10-17 19:12:58 +0300

    f flatten; ...$item

---
+++ Array/spread-operator.php
@@ -0,0 +1,28 @@
+<?php 
+/* $frenchCities = ['paris', 'marseille'];
+$cities = ['milan', 'rome', ...$frenchCities];
+//то же самое
+$frenchCities = ['paris', 'marseille'];
+$cities = array_merge(['milan', 'rome'], $frenchCities); */
+//также;
+/* $cities = ['milan', ... $frenchCities, 'rome'];
+$cities = array_merge(['milan', $frenchCities, ['rome']]);
+
+$frenchCities = ['paris', 'marseille'];
+$italianCities = ['rome', 'milan']; 
+$cities = [...$frenchCities, ...$italianCities]; */
+?>
+
+<?php
+function flatten($coll) 
+{
+    $result = [];
+    foreach ($coll as $item) {
+        if (is_array($item)) {
+        $result = [...$result, ...$item];
+        } else {
+            $result[] = $item;
+        }      
+    }
+    return $result;
+}
